970478ba0c65a8585299d4947a39eb26fc2e6ffd
shikunyao <44891725+shikunyaoa@users.noreply.github.com>
2022-04-29 14:30:59 +0800

    eureka example

---
+++ eureka-server/src/main/java/com/sky/cloud/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.sky.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//启动eureka注册中心功能
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
