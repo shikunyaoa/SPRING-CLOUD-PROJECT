769262d0214a69a3f117723255b4b5f30f3d1f42
shikunyao <44891725+shikunyaoa@users.noreply.github.com>
2022-05-30 17:15:58 +0800

    eureka example

---
+++ config-client/src/main/java/com/sky/cloud/configclient/controller/MessageRestController.java
@@ -0,0 +1,25 @@
+package com.sky.cloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sky
+ * @date 2022/5/30
+ * @description
+ */
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+     String getMessage() {
+        return this.message;
+    }
+}
